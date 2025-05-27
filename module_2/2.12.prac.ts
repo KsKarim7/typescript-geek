b1831b1bad5d194d65f83032161026ae978dc6d0
Khaled Karim <kskarim177@gmail.com>
2025-05-27 22:54:12 +0600

    practice problems

--- module_2/2.12.prac.ts
+++ module_2/2.12.prac.ts
@@ -1,4 +1,5 @@
 {
+    // https://github.com/Apollo-Level2-Web-Dev/batch-4-be-a-typescript-technocrat/blob/main/practice_probelms.md  -- Question
     console.log('Hello');
 
     function designation(name: string, age: number, role?: 'admin' | 'user' | 'guest') {
